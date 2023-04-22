8214cd52dcb32690a31e42f252bb9f721fb5cec4
hxxnxxa <hyunhw92@hanmail.net>
2023-04-22 13:40:32 +0900

    DELETE: comment

--- txt2csv.py
+++ txt2csv.py
@@ -1,9 +1,10 @@
 import csv
 import os
 
+
+# export 디렉토리 없으면 생성
 output_dir = 'output'
 if not os.path.exists(output_dir):
     os.makedirs(os.path.join(output_dir))
 
-JONNA TIRED
-TO BE CONTINUE
+
